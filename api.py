1895a4ae29f0d020dd326d26a152ac99ef24dc06
William Ricardo Santos Lima <williamrsl@pbtech.net.br>
2022-12-26 11:14:51 -0300

    first commit

---
+++ api.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+import ast
+
+app = Flask(__name__);
+api = Api(app);
+
+class Users(Resource):
+    def get(self):
+        return 'Hello'
+
+api.add_resource(Users, '/users')
+
+if __name__ == "__main__":
+    app.run(debug=True)
